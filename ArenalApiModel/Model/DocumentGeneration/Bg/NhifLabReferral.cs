f144ea2a9d91688e89ef5615e868910881f53a23
Ivan-Kalatchev <mani.munana@gmail.com>
2023-03-30 23:34:40 +0300

    нхис реферрал

--- ArenalApiModel/Model/DocumentGeneration/Bg/NhifLabReferral.cs
+++ ArenalApiModel/Model/DocumentGeneration/Bg/NhifLabReferral.cs
@@ -6,6 +6,45 @@
     /// </summary>
     public class NhifLabReferral
     {
+
+        #region Referral Metadata
+
+        /// <summary>
+        /// The bulgarian national reference number for the referral.
+        /// </summary>
+        public string Nrn { get; set; }
+
+        /// <summary>
+        /// The LRN number for the referral.
+        /// </summary>
+        public string Lrn { get; set; }
+
+        #endregion
+
+        #region Patient Data
+
+        /// <summary>
+        /// The patient that the referral is targeted to.
+        /// </summary>
+        public Patient Patient { get; set; }
+
+        #endregion
+
+        #region Issuer Doctor Data
+
+        /// <summary>
+        /// The referral issuer's information.
+        /// </summary>
+        public Doctor Issuer { get; set; }
+
+        #endregion
+
+        #region Laboratory Data
+
+        public Doctor LaboratoryDocotr { g}
+
+        #endregion
+
     }
 
 }
