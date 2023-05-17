d21207f46ef1dc020932a0be0ca22d0b8f94ec31
Konstantin Atanasov <konstantin.kirov.atanasov@gmail.com>
2023-05-18 00:55:35 +0300

    L10n Address

--- ArenalApiModel/Model/Address.cs
+++ ArenalApiModel/Model/Address.cs
@@ -13,47 +13,64 @@ public class Address
     /// <summary>
     /// Two-letter country code, according to ISO 3166-1 alpha 2.
     /// </summary>
-    [Display(GroupName = "Address", ShortName = "Code", Name = "CountryCode",
-        Description = "Country code of the address country.",
-        Prompt = "Please, enter a valid country code, according to ISO 3166-1 alpha 2.")]
+    [Display(GroupName = nameof(L10n.Address.AddressGroupName),
+        ShortName = nameof(L10n.Address.CountryCodeShortName),
+        Name = nameof(L10n.Address.CountryCodeName),
+        Description = nameof(L10n.Address.CountryCodeDescription),
+        Prompt = nameof(L10n.Address.CountryCodePrompt),
+        ResourceType = typeof(L10n.Address))]
     public string CountryCode { get; set; }
 
     /// <summary>
     /// State or Region.
     /// </summary>
-    [Display(GroupName = "Address", ShortName = "Region", Name = "State/Region",
-        Description = "Region in the country.",
-        Prompt = "Please, enter country region.")]
+    [Display(GroupName = nameof(L10n.Address.AddressGroupName),
+        ShortName = nameof(L10n.Address.RegionShortName),
+        Name = nameof(L10n.Address.RegionName),
+        Description = nameof(L10n.Address.RegionDescription),
+        Prompt = nameof(L10n.Address.RegionPrompt),
+        ResourceType = typeof(L10n.Address))]
     public string Region { get; set; }
 
     /// <summary>
     /// Name of the city/town.
     /// </summary>
-    [Display(GroupName = "Address", ShortName = "City", Name = "City/Town",
-        Description = "City",
-        Prompt = "Please, enter city.")]
+    [Display(GroupName = nameof(L10n.Address.AddressGroupName),
+        ShortName = nameof(L10n.Address.CityShortName),
+        Name = nameof(L10n.Address.CityName),
+        Prompt = nameof(L10n.Address.CityPrompt),
+        ResourceType = typeof(L10n.Address))]
     public string City { get; set; }
 
     /// <summary>
     /// Postal code.
     /// </summary>
-    [Display(GroupName = "Address", ShortName = "PostCode", Name = "Postal code",
-        Description = "Post code of the address",
-        Prompt = "Please, enter address post code.")]
+    [Display(GroupName = nameof(L10n.Address.AddressGroupName),
+        ShortName = nameof(L10n.Address.PostCodeShortName),
+        Name = nameof(L10n.Address.PostCodeName),
+        Description = nameof(L10n.Address.PostCodeDescription),
+        Prompt = nameof(L10n.Address.PostCodePrompt),
+        ResourceType = typeof(L10n.Address))]
     public string PostCode { get; set; }
 
     /// <summary>
     /// Actual address, e.g. "10 Downing St".
     /// </summary>
-    [Display(GroupName = "Address", ShortName = "Street", Name = "Street address",
-        Description = "Street",
-        Prompt = "Please, enter street address.")]
+    [Display(GroupName = nameof(L10n.Address.AddressGroupName),
+        ShortName = nameof(L10n.Address.StreetAddressShortName),
+        Name = nameof(L10n.Address.StreetAddressName),
+        Description = nameof(L10n.Address.StreetAddressDescription),
+        Prompt = nameof(L10n.Address.StreetAddressPrompt),
+        ResourceType = typeof(L10n.Address))]
     public string StreetAddress { get; set; }
 
     /// <summary>
     /// Geographic location.
     /// </summary>
-    [Display(GroupName = "Coordinates", ShortName = "Coordinates", Name = "Geographic location")]
+    [Display(GroupName = nameof(L10n.Address.AddressGroupName),
+        ShortName = nameof(L10n.Address.StreetAddressPrompt),
+        Name = nameof(L10n.Address.CoordinatesName),
+        ResourceType = typeof(L10n.Address))]
     public GeoCoordinates Coordinates { get; set; }
 
 
